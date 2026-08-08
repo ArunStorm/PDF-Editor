ed424dcee13275c0918e3cf2b50d73664788b5d8
Arunkumar <arunkumar19962000@gmail.com>
2026-08-09 02:28:21 +0530

    test: map TypeScript ESM .js imports for Jest

--- packages/pdf-core/jest.config.cjs
+++ packages/pdf-core/jest.config.cjs
@@ -1,1 +1,9 @@
-module.exports = { preset: 'ts-jest', testEnvironment: 'node', roots: ['<rootDir>/src'], moduleNameMapper: { '^@pdf-editor/(.*)$': '<rootDir>/../$1/src' } };
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '^@pdf-editor/(.*)$': '<rootDir>/../$1/src',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
