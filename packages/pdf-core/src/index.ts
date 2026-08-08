dbfe4377da5f08bd810c522f419e92113e01f0ed
Arunkumar <arunkumar19962000@gmail.com>
2026-08-09 01:52:20 +0530

    feat(pdf-core): export form helpers

--- packages/pdf-core/src/index.ts
+++ packages/pdf-core/src/index.ts
@@ -1,11 +1,6 @@
 import { open, renderPage, annotate, save, ocrPage } from './pdfCoreStub.js';
 export * from './pdfCoreStub.js';
 export * from './pdfTools.js';
+export * from './forms.js';
 
-export const pdfCore = {
-  open,
-  renderPage,
-  annotate,
-  save,
-  ocrPage,
-};
+export const pdfCore = { open, renderPage, annotate, save, ocrPage };
