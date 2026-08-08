fde56ae900166911145176d296425c303be36255
Arunkumar <arunkumar19962000@gmail.com>
2026-08-09 01:51:41 +0530

    feat(pdf-core): export PDF organization tools

--- packages/pdf-core/src/index.ts
+++ packages/pdf-core/src/index.ts
@@ -1,3 +1,11 @@
 import { open, renderPage, annotate, save, ocrPage } from './pdfCoreStub.js';
 export * from './pdfCoreStub.js';
-export const pdfCore = { open, renderPage, annotate, save, ocrPage };
+export * from './pdfTools.js';
+
+export const pdfCore = {
+  open,
+  renderPage,
+  annotate,
+  save,
+  ocrPage,
+};
