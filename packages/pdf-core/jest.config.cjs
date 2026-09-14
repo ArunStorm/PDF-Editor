f850babd4a2ba32582336608a6a07faaf0530e97
Arunkumar <arunkumar19962000@gmail.com>
2026-09-14 12:11:52 +0530

    chore: remove deprecated ts-jest configuration

--- packages/pdf-core/jest.config.cjs
+++ packages/pdf-core/jest.config.cjs
@@ -3,11 +3,11 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true, isolatedModules: true }],
+  },
   moduleNameMapper: {
     '^@pdf-editor/(.*)$': '<rootDir>/../$1/src',
     '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  globals: {
-    'ts-jest': { useESM: true },
   },
 };
